1e44b48d5c89fb6db01394818b45aed11b3b4310
Micah Levine <micah.levine@outlook.com>
2019-12-01 00:13:50 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 20, "Male", "Brown", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "Male", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 6, "Female", "Black", "Goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 1, "Male", "Brown", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 13, "Female", "Black", "Goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 3, "Male", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 6, "Female", "Brown", "Happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 7, "Male", "Brown", "Angry", 0);
